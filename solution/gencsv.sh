893b08d1a002e9008e57f7106ed106dabb34dc03
Mayukh <mayukh.rooz@gmail.com>
2021-03-16 19:13:39 +0530

    Update gencsv.sh

--- solution/gencsv.sh
+++ solution/gencsv.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 ENTRIES=$1 
-for i in $(seq 0 $ENTRIES); do echo "$i, $((1 + $RANDOM % 10000))";done > inputFile
+for i in $(seq 0 $ENTRIES); do echo "$i, $((1 + $RANDOM % 100000))";done > inputFile
 
 
